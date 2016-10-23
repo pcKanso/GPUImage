7159341d3cb456dafc60204450aa3b6ee6550a52
pcKanso <paulcausey@gmail.com>
2016-10-23 00:05:32 -0700

    Fixed issues -- hopefully -- with audio Sync and crashing ("Problem appending pixel buffer", "Couldn't write a frame")

--- framework/Source/GPUImageVideoCamera.m
+++ framework/Source/GPUImageVideoCamera.m
@@ -959,35 +959,38 @@ NSString *const kGPUImageYUVVideoRangeConversionForLAFragmentShaderString = SHAD
 
 - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection
 {
-    if (!self.captureSession.isRunning)
-    {
-        return;
-    }
-    else if (captureOutput == audioOutput)
-    {
-        [self processAudioSampleBuffer:sampleBuffer];
-    }
-    else
-    {
-        if (dispatch_semaphore_wait(frameRenderingSemaphore, DISPATCH_TIME_NOW) != 0)
-        {
-            return;
-        }
-        
-        CFRetain(sampleBuffer);
-        runAsynchronouslyOnVideoProcessingQueue(^{
-            //Feature Detection Hook.
-            if (self.delegate)
-            {
-                [self.delegate willOutputSampleBuffer:sampleBuffer];
-            }
-            
-            [self processVideoSampleBuffer:sampleBuffer];
-            
-            CFRelease(sampleBuffer);
-            dispatch_semaphore_signal(frameRenderingSemaphore);
-        });
-    }
+	@synchronized(self)
+	{
+		if (!self.captureSession.isRunning)
+		{
+			return;
+		}
+		else if (captureOutput == audioOutput)
+		{
+			[self processAudioSampleBuffer:sampleBuffer];
+		}
+		else
+		{
+			if (dispatch_semaphore_wait(frameRenderingSemaphore, DISPATCH_TIME_NOW) != 0)
+			{
+				return;
+			}
+			
+			CFRetain(sampleBuffer);
+			runAsynchronouslyOnVideoProcessingQueue(^{
+				//Feature Detection Hook.
+				if (self.delegate)
+				{
+					[self.delegate willOutputSampleBuffer:sampleBuffer];
+				}
+				
+				[self processVideoSampleBuffer:sampleBuffer];
+				
+				CFRelease(sampleBuffer);
+				dispatch_semaphore_signal(frameRenderingSemaphore);
+			});
+		}
+	}
 }
 
 #pragma mark -
